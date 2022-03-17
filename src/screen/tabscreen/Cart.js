1ca14a99dfa0e9b69ce692870588e4a733006a63
Shaharukh khan <Shaharukhskb@gmail.com>
2022-03-17 22:24:22 +0530

    c

--- src/screen/tabscreen/Cart.js
+++ src/screen/tabscreen/Cart.js
@@ -1,12 +1,32 @@
-import { Text, View } from 'react-native'
+import { Text, View,ImageBackground,Dimensions ,TouchableOpacity,Image,TextInput} from 'react-native'
 import React, { Component } from 'react'
+const { width, height } = Dimensions.get("window");
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import * as Animatable from 'react-native-animatable';
+import { Images } from "../../theme/index";
+import {
+  AppButton,
+  LinkText,
+  FloatingLabelInput,
+  BackButtonHeader,
+  BackButtonHeader1
+} from '../../components/index';
 
 export default class Cart extends Component {
   render() {
     return (
-      <View>
-        <Text>Cart</Text>
-      </View>
+     <View style={{flex:1}}>
+ <BackButtonHeader
+    Color={"#FFFFFFF"}
+    backPressed={() => { this.props.navigation.goBack() }}
+     />
+     <View style={{paddingHorizontal:width*0.05}}>
+       <Image
+       source={require('../../images/banner1.png')}
+       style={{height:height*0.24,width:width*0.92,alignSelf:"center"}} resizeMode={'contain'}
+       />
+     </View>
+     </View>
     )
   }
 }
