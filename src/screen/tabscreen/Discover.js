2356f73db3073d777c0042bd5633702aba0e610d
Shaharukh khan <Shaharukhskb@gmail.com>
2022-03-24 20:16:28 +0530

    c

--- src/screen/tabscreen/Discover.js
+++ src/screen/tabscreen/Discover.js
@@ -1,82 +1,44 @@
-import * as React from 'react';
-import { Animated, View, TouchableOpacity, StyleSheet,Text } from 'react-native';
+import { Switch,Animated,Text, View,ImageBackground,Dimensions ,TouchableOpacity,Image,TextInput,ScrollView,StyleSheet} from 'react-native'
+import React, { Component } from 'react'
+const { width, height } = Dimensions.get("window");
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import * as Animatable from 'react-native-animatable';
+import FIcon from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { Images } from "../../theme/index";
 import { TabView, SceneMap } from 'react-native-tab-view';
+import {
+  AppButton,
+  LinkText,
+  FloatingLabelInput,
+  BackButtonHeader,
+  BackButtonHeader1
+} from '../../components/index';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const FirstRoute = () => (
-  <View style={[styles.container, { backgroundColor: '#ff4081' }]} >
-    <Text>
-      Hello world
-    </Text>
-    </View>
-);
-const SecondRoute = () => (
-  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
-);
-
-export default class Discover extends React.Component {
-  state = {
-    index: 0,
-    routes: [
-      { key: 'first', title: 'First' },
-      { key: 'second', title: 'Second' },
-    ],
-  };
-
-  _handleIndexChange = (index) => this.setState({ index });
-
-  _renderTabBar = (props) => {
-    const inputRange = props.navigationState.routes.map((x, i) => i);
-
-    return (
-      <View style={styles.tabBar}>
-        {props.navigationState.routes.map((route, i) => {
-          const opacity = props.position.interpolate({
-            inputRange,
-            outputRange: inputRange.map((inputIndex) =>
-              inputIndex === i ? 1 : 0.5
-            ),
-          });
-
-          return (
-            <TouchableOpacity
-              style={styles.tabItem}
-              onPress={() => this.setState({ index: i })}>
-              <Animated.Text style={{ opacity }}>{route.title}</Animated.Text>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-    );
-  };
-
-  _renderScene = SceneMap({
-    first: FirstRoute,
-    second: SecondRoute,
-  });
-
+export default class Discover extends Component {
   render() {
     return (
-      <TabView
-        navigationState={this.state}
-        renderScene={this._renderScene}
-        renderTabBar={this._renderTabBar}
-        onIndexChange={this._handleIndexChange}
-      />
-    );
+      <SafeAreaView style={{flex:1}}>
+      <TouchableOpacity onPress={() => { this.props.navigation.goBack() }}>
+      <View style={{width: '100%',height: '100%'}}>
+        
+      <View style={{width: '100%',height: '10%', backgroundColor: '#fdfdfk',paddingHorizontal:width*0.01,
+
+      alignItems:'center',justifyContent:'flex-start',flexDirection:'row'}}>
+        
+     <Ionicons name="chevron-back-outline" color={"#E44217"} size={width*0.07}
+     />
+     <Text style={{fontSize:width*0.06,fontWeight:'bold',color:"#E44217"}}>Back</Text>
+   
+   </View>
+   
+   </View>
+   </TouchableOpacity>
+   </SafeAreaView>
+ 
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  tabBar: {
-    flexDirection: 'row',
-    paddingTop: 44,
-  },
-  tabItem: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 16,
-  },
-});
