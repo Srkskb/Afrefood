5fd55dfcd66dc26039e888d68eac1ff4663dd8cc
Shaharukh khan <Shaharukhskb@gmail.com>
2022-05-11 13:23:35 +0530

    c

--- src/screen/food_cart/FoodCart.js
+++ src/screen/food_cart/FoodCart.js
@@ -15,7 +15,7 @@ import { TabView, SceneMap } from 'react-native-tab-view';
 export default class FoodCart extends Component {
     constructor(props) {
         super(props)
-        this.state = { count: 0 }
+        this.state = { count: 1 }
     }
   render() {
     return (
@@ -87,23 +87,181 @@ export default class FoodCart extends Component {
       </View>
       <View style={{marginHorizontal:width*0.03,paddingTop:width*0.03}}>
       <View elevation ={5} style={{height:100,borderRadius:10,backgroundColor:"#fff"}}>
-          <View style={{flexDirection:'row',justifyContent:'space-around',alignItems:'center',width:'20%'}}>
-              <View style={{paddingTop:width*0.02}}>
+          <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+              <View style={{paddingTop:width*0.013,paddingHorizontal:width*0.03}}>
 <View style={{flexDirection:'column',alignItems:'center',justifyContent:'space-between',borderWidth:1,padding:10,borderRadius:10}}>
+  <TouchableOpacity onPress={()=>this.setState({count:this.state.count+1})}>
 <Text style={{fontSize:width*0.04,fontWeight:'bold'}}>
     +
 </Text>
+</TouchableOpacity>
 <Text style={{fontSize:width*0.04,fontWeight:'bold'}}>
-    1
+ {this.state.count}
 </Text>
+<TouchableOpacity onPress={()=>{this.state.count>1? this.setState({count:this.state.count-1}):this.state.count}}>
 <Text style={{fontSize:width*0.04,fontWeight:'bold'}}>
     -
 </Text>
+</TouchableOpacity>
 </View>
+
+</View>
+<View style={{justifyContent:'center',alignItems:'center'}}>
+<Image
+source={require('../../images/img1.jpg')}
+style={{height:height*0.06,width:width*0.3}}
+/>
+</View>
+<View style={{flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+<Text style={{fontSize:width*0.05,color:"#000",fontFamily:'Poppins-SemiBold'}}>
+  Noodles
+</Text>
+<Text style={{fontSize:width*0.035,color:"#000",fontFamily:'Poppins-SemiBold'}}>
+ {'$22.5'}
+</Text>
+</View>
+<View style={{paddingHorizontal:width*0.04,paddingTop:width*0.02,paddingRight:width*0.05,alignItems:'flex-end'}}>
+  <TouchableOpacity onPress={()=>alert("Are you sure want to delete")}>
+<Entypo
+name='squared-cross' size={width*0.05} color={"#DCDCDC"}
+/>
+</TouchableOpacity>
 </View>
           </View>
       </View>
       </View>
+      <View style={{marginHorizontal:width*0.03,paddingTop:width*0.03}}>
+      <View elevation ={5} style={{height:100,borderRadius:10,backgroundColor:"#fff"}}>
+          <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+              <View style={{paddingTop:width*0.013,paddingHorizontal:width*0.03}}>
+<View style={{flexDirection:'column',alignItems:'center',justifyContent:'space-between',borderWidth:1,padding:10,borderRadius:10}}>
+  <TouchableOpacity onPress={()=>this.setState({count:this.state.count+1})}>
+<Text style={{fontSize:width*0.04,fontWeight:'bold'}}>
+    +
+</Text>
+</TouchableOpacity>
+<Text style={{fontSize:width*0.04,fontWeight:'bold'}}>
+ {this.state.count}
+</Text>
+<TouchableOpacity onPress={()=>{this.state.count>1? this.setState({count:this.state.count-1}):this.state.count}}>
+<Text style={{fontSize:width*0.04,fontWeight:'bold'}}>
+    -
+</Text>
+</TouchableOpacity>
+</View>
+
+</View>
+<View style={{justifyContent:'center',alignItems:'center'}}>
+<Image
+source={require('../../images/img20.png')}
+style={{height:height*0.06,width:width*0.3}}
+/>
+</View>
+<View style={{flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+<Text style={{fontSize:width*0.05,color:"#000",fontFamily:'Poppins-SemiBold'}}>
+  Pizza
+</Text>
+<Text style={{fontSize:width*0.035,color:"#000",fontFamily:'Poppins-SemiBold'}}>
+ {'$12.5'}
+</Text>
+</View>
+<View style={{paddingHorizontal:width*0.04,paddingTop:width*0.02,paddingRight:width*0.05,alignItems:'flex-end'}}>
+  <TouchableOpacity onPress={()=>alert("Are you sure want to delete")}>
+<Entypo
+name='squared-cross' size={width*0.05} color={"#DCDCDC"}
+/>
+</TouchableOpacity>
+</View>
+          </View>
+      </View>
+      </View>
+      <View style={{marginHorizontal:width*0.03,paddingTop:width*0.03}}>
+      <View elevation ={5} style={{height:70,borderRadius:10,backgroundColor:"#fff"}}>
+        <View style={{justifyContent:'space-evenly',paddingTop:width*0.03}}>
+          <View style={{flexDirection:'row',justifyContent:'space-around',alignItems:'center'}}>
+            <TextInput 
+            placeholder='Promo code'
+            style={{fontSize:width*0.04,}}
+          />
+          <TouchableOpacity onPress={()=>alert('Apply soon..')}>
+            <View style={{borderRadius:10,borderWidth:1,padding:10,paddingHorizontal:width*0.09,backgroundColor:"#FB2244",borderColor:"#FB2244"}}>
+              <Text style={{fontSize:width*0.04,color:"#FFF"}}>
+              Apply
+              </Text>
+            </View>
+            </TouchableOpacity>
+            </View>
+            </View>
+            </View>
+            </View>
+            <View style={{paddingTop:width*0.05,marginHorizontal:width*0.05}}>
+              <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  Cart total
+</Text>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  $400.00
+</Text>
+              </View>
+
+            </View>
+            <View style={{paddingTop:width*0.05,marginHorizontal:width*0.05}}>
+              <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  Tax
+</Text>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  $40.60
+</Text>
+              </View>
+
+            </View>
+            <View style={{paddingTop:width*0.05,marginHorizontal:width*0.05}}>
+              <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  Delivery
+</Text>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  $4.00
+</Text>
+              </View>
+
+            </View>
+            <View style={{paddingTop:width*0.05,marginHorizontal:width*0.05}}>
+              <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  Promo Discount
+</Text>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+ - $000.00
+</Text>
+              </View>
+
+            </View>
+            <View style={{paddingTop:width*0.05,marginHorizontal:width*0.08}}>
+              <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  Sub Total
+</Text>
+<Text style={{fontSize:width*0.035,color:'#000',fontFamily:'Poppins-SemiBold'}}>
+  $444.60
+</Text>
+              </View>
+
+            </View>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate("Payment")}>
+            <View style={{paddingTop:width*0.05,marginHorizontal:width*0.06}}>
+              <View style={{borderWidth:1,borderRadius:10,backgroundColor:"#1599B0",borderColor:'#1599B0',justifyContent:'center',alignItems:'center',padding:10}}>
+<Text style={{color:'#fff',fontSize:width*0.04}}>
+  Procced to checkout
+</Text>
+              </View>
+
+            </View>
+            </TouchableOpacity>
+            <View style={{paddingBottom:width*0.09}}>
+
+            </View>
       </ScrollView>
       </View>
     )
